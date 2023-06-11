1c87cdca0fffff4f2098654ec88a894388aedd4a
wpmohammad1 <alimohammadjhd@gmail.com>
2023-06-11 23:21:55 +0600

    install mongodb and dotenv

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require("express")
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// midleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) =>{
+    res.send('summer camp server in running')
+})
+
+app.listen(port, () =>{
+    console.log(`summer camp in running on port: ${port}`)
+})
