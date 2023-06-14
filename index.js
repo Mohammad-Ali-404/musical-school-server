4d5e3a00da93af929393c1556c1c9c1326631b34
wpmohammad1 <alimohammadjhd@gmail.com>
2023-06-14 20:39:33 +0600

    create make admin api

--- index.js
+++ index.js
@@ -49,6 +49,20 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result)
     })
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
 
     app.get('/class', async(req, res) =>{
       const result = await classCollection.find().toArray()
