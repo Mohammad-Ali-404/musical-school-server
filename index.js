8792d8446a48a1141db0926f7e04eda4c0c720f1
wpmohammad1 <alimohammadjhd@gmail.com>
2023-06-14 17:23:48 +0600

    create carts delete api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e60xkn0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,15 +32,29 @@ async function run() {
     const instructorCollection = client.db('summerCampDb').collection('instructor')
     const cartCollection = client.db('summerCampDb').collection('carts')
 
+
+
+    app.get('/users', async(req, res) =>{
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
     app.post('/users', async(req, res) =>{
       const user = req.body;
+      const query = {email : user.email}
+      const exixtingUser = await userCollection.findOne(query)
+      if (exixtingUser) {
+        return res.send({message: 'user already exists'})
+      }
       const result = await userCollection.insertOne(user);
       res.send(result)
     })
+
     app.get('/class', async(req, res) =>{
       const result = await classCollection.find().toArray()
       res.send(result)
     })
+    
     app.get('/instructor', async(req, res) =>{
       const result = await instructorCollection.find().toArray()
       res.send(result)
@@ -57,11 +71,16 @@ async function run() {
       const result = await cartCollection.find(query).toArray()
       res.send(result)
     })
-
     app.post('/carts', async (req, res) =>{
       const item = req.body;
       console.log(item)
       const result = await cartCollection.insertOne(item);
+      res.send(result)
+    })
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
     // Send a ping to confirm a successful connection
